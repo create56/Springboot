67c114ad30e19b107eeaaa67fc9f2aa4b84fc256
DESKTOP-NUOPUIF\ITPS <yky2496@naver.com>
2021-06-22 17:38:28 +0900

    스프링 실습(1)

---
+++ secondapp/src/main/java/com/cos/secondapp/SecondappApplication.java
@@ -0,0 +1,12 @@
+package com.cos.secondapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondappApplication.class, args);
+	}
+}
