ff98f744d8f74ff478463830aa8e4c529cfca605
DESKTOP-NUOPUIF\ITPS <yky2496@naver.com>
2021-06-24 17:53:56 +0900

    스프링 실습3

--- newapp/src/main/java/com/korea/newapp/HomeController.java
+++ newapp/src/main/java/com/korea/newapp/HomeController.java
@@ -1,17 +1,40 @@
 package com.korea.newapp;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	
+	final HttpServletRequest request;
+	final HttpSession session;
+	
+	// DI = Dependency Injection(의존성 주입)
+	public HomeController(HttpServletRequest request, HttpSession session) {
+		this.request = request;
+		this.session = session;
+	}
+
 	@GetMapping("/home")
-	public String home() {
-		return"home";
+	public String home(Model model) {
+		if (session != null) {
+			System.out.println("세션 널 아니에요");
+		}
+			model.addAttribute("data","홍길동");
+			System.out.println(request.getMethod());
+//			String rData = "request 데이터";
+//			String sData = "session 데이터";
+//			request.setAttribute("data", rData);
+//			session.setAttribute("data", sData);
+		return"home"; 
 	}
 	
 	@GetMapping("/post")
 	public String post() {
-		return"post";
+		return"redirect:/home";
 	}
 }
